ed904a388144a6fc2816ecbadc29ef5b3ce17051
Michael Crismali <michael.crismali@gmail.com>
2015-04-25 16:49:45 -0500

    moved product test class to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,68 @@ require "database_cleaner"
 require "shoulda-matchers"
 require "yaml"
 
+VALUE_FIELDS = [:count, :name, :price]
+TYPED_FIELDS = {
+  title: :string,
+  name_value: :value,
+  id_value: :value,
+  external_id: :integer,
+  admin: :boolean,
+  approved_on: :date,
+  reviewed_at: :date_time,
+  precision: :decimal,
+  reset_at: :time,
+  amount_floated: :float,
+  sequential_data: :array,
+  things: :json,
+  stuff: :jsonb,
+  a_lot_of_things: :json_array,
+  a_lot_of_stuff: :jsonb_array,
+  nicknames: :string_array,
+  rankings: :integer_array,
+  favorited_history: :boolean_array,
+  login_days: :date_array,
+  favorites_at: :date_time_array,
+  prices: :decimal_array,
+  login_times: :time_array,
+  amounts_floated: :float_array,
+  document: {
+    nested: {
+      values: :array,
+      are: :string
+    },
+    here: :string
+  }
+}
+ALL_FIELDS = VALUE_FIELDS + TYPED_FIELDS.keys
+
+class Product < ActiveRecord::Base
+  jsonb_accessor :options, *VALUE_FIELDS, TYPED_FIELDS
+end
+
+class OtherProduct < ActiveRecord::Base
+  self.table_name = "products"
+  jsonb_accessor :options, title: :string, document: { nested: { are: :string } }
+
+  def options=(value)
+    value["title"] = "new title"
+    super
+  end
+
+  def title=(value)
+    super(value.try(:upcase))
+  end
+
+  def title
+    super.try(:downcase)
+  end
+
+  def reload
+    super
+    :wrapped
+  end
+end
+
 RSpec::Matchers.define :alias_the_method do |method_name|
   match do |actual|
     if actual.respond_to?(method_name) && actual.respond_to?(@other_method_name)
