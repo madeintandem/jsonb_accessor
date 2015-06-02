f8f1957962a19cfcecf8f6c2e6c1a396b17d1584
Joe Hirn <joseph.hirn@gmail.com>
2015-06-02 09:21:23 -0500

    Refactor jsonb_accessor macro into smaller private methods.

--- lib/jsonb_accessor/macro.rb
+++ lib/jsonb_accessor/macro.rb
@@ -3,10 +3,11 @@ module JsonbAccessor
     module ClassMethods
       def jsonb_accessor(jsonb_attribute, *value_fields, **typed_fields)
         fields_map = JsonbAccessor::FieldsMap.new(value_fields, typed_fields)
-
         class_namespace = ClassBuilder.generate_class_namespace(name)
         attribute_namespace = ClassBuilder.generate_attribute_namespace(jsonb_attribute, class_namespace)
         nested_classes = ClassBuilder.generate_nested_classes(attribute_namespace, fields_map.nested_fields)
+        jsonb_attribute_initialization_method_name = "initialize_jsonb_attrs_for_#{jsonb_attribute}"
+        jsonb_attribute_scope_name = "#{jsonb_attribute}_contains"
 
         singleton_class.send(:define_method, "#{jsonb_attribute}_classes") do
           nested_classes
@@ -14,39 +15,56 @@ module JsonbAccessor
 
         delegate "#{jsonb_attribute}_classes", to: :class
 
-        jsonb_attribute_initialization_method_name = "initialize_jsonb_attrs_for_#{jsonb_attribute}"
+        _initialize_jsonb_attrs(jsonb_attribute, fields_map, jsonb_attribute_initialization_method_name)
+        _create_jsonb_attribute_scope_name(jsonb_attribute, jsonb_attribute_scope_name)
+        _create_jsonb_scopes(fields_map, jsonb_attribute_scope_name)
+        _create_jsonb_accessor_methods(jsonb_attribute, jsonb_attribute_initialization_method_name, fields_map)
+      end
 
+      private
+
+      def _initialize_jsonb_attrs(jsonb_attribute, fields_map, jsonb_attribute_initialization_method_name)
         define_method(jsonb_attribute_initialization_method_name) do
           jsonb_attribute_hash = send(jsonb_attribute) || {}
           fields_map.names.each do |field|
             send("#{field}=", jsonb_attribute_hash[field.to_s])
           end
         end
-
         after_initialize(jsonb_attribute_initialization_method_name)
+      end
 
-        attribute_scope = lambda do |attributes|
-          query_options = new(attributes).send(jsonb_attribute)
-          fields = attributes.keys.map(&:to_s)
-          query_options.delete_if { |key, value| fields.exclude?(key) }
-          query_json = TypeHelper.type_cast_as_jsonb(query_options)
-          where("#{table_name}.#{jsonb_attribute} @> ?", query_json)
-        end
-        jsonb_attribute_scope_name = "#{jsonb_attribute}_contains"
-        scope jsonb_attribute_scope_name, attribute_scope
+      def _create_jsonb_attribute_scope_name(jsonb_attribute, jsonb_attribute_scope_name)
+        scope jsonb_attribute_scope_name, (lambda do |attributes|
+                                             query_options = new(attributes).send(jsonb_attribute)
+                                             fields = attributes.keys.map(&:to_s)
+                                             query_options.delete_if { |key, value| fields.exclude?(key) }
+                                             query_json = TypeHelper.type_cast_as_jsonb(query_options)
+                                             where("#{table_name}.#{jsonb_attribute} @> ?", query_json)
+                                           end)
+      end
 
+      def _create_jsonb_scopes(fields_map, jsonb_attribute_scope_name)
+        __create_jsonb_standard_scopes(fields_map, jsonb_attribute_scope_name)
+        __create_jsonb_boolean_scopes(fields_map)
+      end
+
+      def __create_jsonb_standard_scopes(fields_map, jsonb_attribute_scope_name)
         fields_map.names.each do |field|
           scope "with_#{field}", -> (value) { send(jsonb_attribute_scope_name, field => value) }
         end
+      end
 
-        typed_fields.each do |field, type|
+      def __create_jsonb_boolean_scopes(fields_map)
+        fields_map.typed_fields.each do |field, type|
           case type
           when :boolean
             scope "is_#{field}", -> { send("with_#{field}", true) }
             scope "not_#{field}", -> { send("with_#{field}", false) }
           end
         end
+      end
 
+      def _create_jsonb_accessor_methods(jsonb_attribute, jsonb_attribute_initialization_method_name, fields_map)
         jsonb_accessor_methods = Module.new do
           define_method("#{jsonb_attribute}=") do |value|
             write_attribute(jsonb_attribute, value)
@@ -60,6 +78,12 @@ module JsonbAccessor
           end
         end
 
+        __create_jsonb_typed_field_setters(jsonb_attribute, jsonb_accessor_methods, fields_map)
+        __create_jsonb_nested_field_accessors(jsonb_attribute, jsonb_accessor_methods, fields_map)
+        include jsonb_accessor_methods
+      end
+
+      def __create_jsonb_typed_field_setters(jsonb_attribute, jsonb_accessor_methods, fields_map)
         fields_map.typed_fields.each do |field, type|
           attribute(field.to_s, TypeHelper.fetch(type))
 
@@ -71,13 +95,14 @@ module JsonbAccessor
             end
           end
         end
+      end
 
+      def __create_jsonb_nested_field_accessors(jsonb_attribute, jsonb_accessor_methods, fields_map)
         fields_map.nested_fields.each do |field, nested_attributes|
           attribute(field.to_s, TypeHelper.fetch(:value))
-
           jsonb_accessor_methods.instance_eval do
             define_method("#{field}=") do |value|
-              instance_class = nested_classes[field]
+              instance_class = send("#{jsonb_attribute}_classes")[field]
               instance = cast_nested_field_value(value, instance_class, __method__)
 
               new_jsonb_value = (send(jsonb_attribute) || {}).merge(field.to_s => instance.attributes)
@@ -86,8 +111,6 @@ module JsonbAccessor
             end
           end
         end
-
-        include jsonb_accessor_methods
       end
     end
   end
