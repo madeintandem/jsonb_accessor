21aa0ef09b415ef27381713d19755cce7b891af8
Jakob Pupke <jakop@pm.me>
2021-05-14 19:02:31 +0200

    fix performance test

--- spec/jsonb_accessor_performance_spec.rb
+++ spec/jsonb_accessor_performance_spec.rb
@@ -5,7 +5,7 @@ require "spec_helper"
 RSpec.describe "Jsonb Accessor Performace" do
   context "initializing objects from the database" do
     before do
-      10.times do
+      1000.times do
         Product.create(
           string_type: "static string",
           integer_type: (1..60_000).to_a.sample,
@@ -20,8 +20,8 @@ RSpec.describe "Jsonb Accessor Performace" do
     it "is of reasonable performance against non-jsonb records" do
       static_time = Benchmark.realtime { StaticProduct.all.to_a }
       jsonb_time = Benchmark.realtime { Product.all.to_a }
-
-      expect(static_time * 10).to be > jsonb_time
+      # it shouldn't even be 0.1 seconds slower to fetch and instantiate 1000 records
+      expect((static_time - jsonb_time).abs).to be < 0.1
     end
   end
 end
